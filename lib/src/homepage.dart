1f07e03be9ad1aee2e10d1ded2adeb9265255993
dragonanish <anishstha5@gmail.com>
2021-06-17 15:48:26 +0545

    day 2 completed

--- lib/src/homepage.dart
+++ lib/src/homepage.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
+  final int day = 30;
+  final String clas = "class tutorial";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Catalog")),
+      appBar: AppBar(
+        title: Text("Catalog"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to the $day day Flutter $clas"),
+        ),
+      ),
     );
   }
 }
